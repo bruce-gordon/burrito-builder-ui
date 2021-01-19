875afcf8573ad412991d1805fdfa939258e13eb7
Bruce Gordon <brc.gordon@gmail.com>
2021-01-19 10:21:13 -0700

    Add additional files

---
+++ src/components/Order/Order.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Order.css';
+
+const Order = ({ id, name, ingredients }) => {
+  console.log(id, name, ingredients)
+
+  return (
+    <div className="order">
+      <h3>{ name }</h3>
+      <ul className="ingredient-list">
+        { ingredients.map((ingredient, index) => {
+          return <li key={index}>{ingredient}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Order;
