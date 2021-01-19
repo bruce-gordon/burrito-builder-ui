e4b9daed504a1eb15c2b09937debb6316795d9a5
Bruce Gordon <brc.gordon@gmail.com>
2021-01-19 10:21:53 -0700

    Update App state with api calls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders, postOrder } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
     getOrders()
+      .then(data => this.setState({ orders: data.orders}))
       .catch(err => console.error('Error fetching:', err));
+  }
+
+  takeOrder = (newOrder) => {
+    postOrder(newOrder)
+    .then(data => this.setState({ orders: [...this.state.orders, data]}))
   }
 
   render() {
@@ -19,7 +28,9 @@ class App extends Component {
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm
+            takeOrder={ this.takeOrder }
+          />
         </header>
 
         <Orders orders={this.state.orders}/>
