1a3e508f88b630ea84d94c084965742b8d461082
Bruce Gordon <brc.gordon@gmail.com>
2021-01-19 12:14:58 -0700

    Test App component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,94 @@
+import React from 'react';
+import App from './App.js'
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { getOrders, postOrder } from '../../apiCalls';
+jest.mock('../../apiCalls');
 
+describe('App', () => {
+
+  beforeEach(() => {
+    getOrders.mockResolvedValueOnce({
+      orders: [
+        {
+            id: 1,
+            name: "Brian",
+            ingredients: [
+                "beans",
+                "carnitas",
+                "queso fresco",
+                "jalapeno"
+            ]
+        },
+        {
+            id: 2,
+            name: "Sammy",
+            ingredients: [
+                "steak",
+                "lettuce",
+                "carnitas",
+                "queso fresco",
+                "jalapeno"
+            ]
+        },
+        {
+            name: "Lucinda",
+            ingredients: [
+                "carnitas",
+                "lettuce",
+                "pico de gallo",
+                "guacamole",
+                "sour cream"
+            ],
+            id: 5
+        }
+      ]
+    })
+
+    postOrder.mockResolvedValueOnce(
+      {
+          id: 4,
+          name: "Leslie",
+          ingredients: [
+              "beans"
+          ]
+      }
+    )
+
+    render (
+      <App />
+    )
+  })
+
+  it('should render a title and form', () => {
+    const title = screen.getByText('Burrito Builder');
+    const nameInput = screen.getByPlaceholderText('Name');
+    const submit = screen.getByText('Submit Order');
+    const beans = screen.getByRole('button', {name: /beans/i})
+    expect(nameInput).toBeInTheDocument();
+    expect(submit).toBeInTheDocument();
+    expect(beans).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+  })
+
+  it('should render order data from api', () => {
+    const cust1 = screen.getByText('Lucinda')
+    const cust2 = screen.getByText('Sammy')
+    const cust3 = screen.getByText('Brian')
+    expect(cust1).toBeInTheDocument()
+    expect(cust2).toBeInTheDocument()
+    expect(cust3).toBeInTheDocument()
+  })
+  //
+  // it('should render new orders', async () => {
+  //   const nameInput = screen.getByPlaceholderText('Name');
+  //   const beans = screen.getByRole('button', {name: /beans/i});
+  //   const submit = screen.getByText('Submit Order');
+  //   userEvent.type(nameInput, 'Leslie');
+  //   userEvent.click(beans);
+  //   userEvent.click(submit);
+  //   const newUser = await screen.getByText('Leslie');
+  //   expect(newUser).toBeInTheDocument();
+  // })
+})
---
+++ src/mockData.js
@@ -0,0 +1,37 @@
+export const mockData =
+{
+  "orders": [
+    {
+        "id": 1,
+        "name": "Brian",
+        "ingredients": [
+            "beans",
+            "carnitas",
+            "queso fresco",
+            "jalapeno"
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Sammy",
+        "ingredients": [
+            "steak",
+            "lettuce",
+            "carnitas",
+            "queso fresco",
+            "jalapeno"
+        ]
+    },
+    {
+        "name": "Lucinda",
+        "ingredients": [
+            "carnitas",
+            "lettuce",
+            "pico de gallo",
+            "guacamole",
+            "sour cream"
+        ],
+        "id": 5
+    }
+  ]
+}
